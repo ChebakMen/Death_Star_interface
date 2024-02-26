5ad925d933c776bd0e4114d0437bab6a8749bdd4
Romeli <butus.5@outlook.com>
2024-02-26 12:15:39 +0700

    last upd

--- src/entities/accountStormtrooper/AccountStormtrooper.tsx
+++ src/entities/accountStormtrooper/AccountStormtrooper.tsx
@@ -1,14 +1,15 @@
 import { FC } from "react";
 import Stormtrooper from "shared/Stormtrooper.webp"
+import img from "shared/icons/ttt.svg"
 
 import "./AccountStormtrooper.css"
 
 const AccountStormtrooper:FC = () =>{
     return (
         <div className="accountStormtrooper__cont">
-            <img src={Stormtrooper} alt="Stormtrooper__img" className="accountStormtrooper__img" />
+            <img src={img } alt="Stormtrooper__img" className="accountStormtrooper__img" />
             <div className="accountStormtrooper__info">
-                <h3 className="accountStormtrooper__name">Штурмовик №29391</h3>
+                <h3 className="accountStormtrooper__name">Штурмовик №707784</h3>
                 <p className="accountStormtrooper__status">Статус: Активный работник</p>
             </div>
             
--- src/widgets/magneticSystem/Magneticsystem.tsx
+++ src/widgets/magneticSystem/Magneticsystem.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import styled from 'styled-components'
 
@@ -7,9 +7,37 @@ import { useState } from 'react'
 import "./Magneticsystem.css"
 import SectionTitle from 'entities/sectionTitle/SectionTitle'
 
+type Inductor = {
+    name: string
+    is_on: boolean
+    durability: number
+    storable_energy: number
+    electromotive_force: number
+    starter_current: number
+    output_current: number
+    current_loss: number
+    q_factor: number
+};
 
 const Magneticsystem: React.FunctionComponent = () => {
-    const [fillPercentage, setFillPercentage] = useState(80);
+
+    const [inductors, updateInductors] = useState<Inductor[] | []>([]); 
+    useEffect(() => {
+        updateInductors([
+           ...inductors,
+            ...Array<Inductor>(24).fill({
+                name: "",
+                is_on: true,
+                durability: 0,
+                storable_energy: 0,
+                electromotive_force: 0,
+                starter_current: 0,
+                output_current: 0,
+                current_loss: 0,
+                q_factor: 0,
+            })
+        ]);
+    }, []);
 
     return (
         <div className="magnetic-system">
@@ -17,30 +45,9 @@ const Magneticsystem: React.FunctionComponent = () => {
             <div className="magnetic-system--container">
                 <p className='magnetic-system--text'>Индукторы</p>
                 <div className="inductors">
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
-                    <div className="magnetic-system--inductor-indicator"></div>
+                    {inductors.map((inductor, ix) => (
+                        <div className="magnetic-system--inductor-indicator"></div>
+                    ))}
                 </div>
                 
             </div>
